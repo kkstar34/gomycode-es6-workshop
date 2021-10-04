15f8145ca3ff684944c968b0bd6088669a58c6b4
kouyate karim <karimkouyate@adjemin.com>
2021-10-04 12:43:26 +0000

    first

---
+++ array.js
@@ -0,0 +1,49 @@
+let array = [15, 24, 15, 26, 1, 8, 45];
+
+
+
+console.log([...array]);
+
+
+// let array1 = array.map((el)=>el*2) //prototype 
+
+// let tab = [];
+// for (let i=0; i<array.length; i++){
+//     tab.push(array[i]*2);
+// }
+
+
+// console.log(tab) ;
+
+
+
+// let array2 = array.filter((el, i)=>el>20*i)
+
+ //let array3 = array.reduce((acc, cur) => acc + cur, 5);
+
+
+// function map (){
+//   let array=[15, 24, 15, 26, 1, 8, 45]
+//   for(let i=0; i< array.length; i++){
+//     array[i]=array[i]+1
+//   }
+//   return array
+// }
+
+
+
+
+// function filter (){
+//   let array=[15, 24, 15, 26, 1, 8, 45]
+//   let tab = []
+//   for(let i=0; i< array.length; i++){
+//     if(array[i]>20){
+//       tab=tab.concat(array[i])
+//     }
+//   }
+//   return tab
+// }
+
+// let combine = [...array, 100, 12]
+
+// console.log(combine)
